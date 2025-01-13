f55a4b360066bc203a01282b5467e9372b30189d
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-01-13 10:26:59 +0100

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+
+  $str = "Laplateforme";
+  $str2 = "vive";
+  $str3 = "!";
+
+  echo $str2 . " " .$str . " " .$str3 ;
+  echo "<br>";
+
+  $val = 6;
+  echo $val;
+  echo "<br>";
+  $val += 4;
+  echo $val;
+  echo "<br>";
+  $myBool = true;
+  echo $myBool;
+  echo "<br>";
+  $myBool = false;
+  echo $myBool;
+  
+
+?>
