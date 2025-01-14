e699fdab9c96380411ade1cf62b78ce5d7f6717b
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-01-14 10:15:39 +0100

    job_2

---
+++ jour02/job02/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+  for ($x = 0; $x <= 1337; $x++) {
+    switch($x) {
+      case 26 :
+        continue;
+      case 37 :
+        continue;
+      case 88 :
+        continue;
+      case 1111 :
+        continue;
+        break;
+      default :
+        echo $x .'<br>';
+    }
+   
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
