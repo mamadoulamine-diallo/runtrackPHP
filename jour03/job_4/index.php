3911980eddda8332ec759e711420e18af5eb3a71
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-01-15 19:29:56 +0100

    job_4_tableaux

---
+++ jour03/job_4/index.php
@@ -0,0 +1,19 @@
+<?php
+  $str = "Dans l'espace, personne ne
+vous entend crier.";
+$count = 0;
+
+for ($i = 0; isset($str[$i]); $i++) {
+   $str[$i];
+   $count++;
+}
+echo $count;
+
+
+
+
+
+
+
+
+?>
