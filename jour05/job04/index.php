bc407368b17739c8b76391240a5846a23bde013c
Lamine Dxallo <mamadou-lamine.diallo@laplateforme.io>
2025-01-17 14:23:46 +0100

    fonction calcule avec 3 parametres

--- jour05/job04/index.php
+++ jour05/job04/index.php
@@ -1,7 +1,31 @@
 <?php 
 function calcule($a,$x, $b) {
-
-
+  $resultat = 0;
+ switch($x) {
+  case '+' :
+    $resultat = $a + $b;
+    break;
+  case '*' :
+    $resultat = $a * $b;
+    break;
+  case '-' :
+    $resultat = $a - $b;
+    break;
+  case '/' :
+    $resultat = $a / $b;
+    break;
+  case '%' :
+    $resultat = $a % $b;
+    break;
+  default:
+  echo 'Operateur Invalide';
+ }
+ return $resultat;
+  
 }
+
+echo calcule(5,'*',2);
+  
+  
 
 ?>
